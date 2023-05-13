fcb9a150b470d87c6aaf24a1fa0184c53ab702e1
Deepakrkt <deepak3640863@gmail.com>
2023-05-13 12:17:11 +0530

    first

--- Armstrong/src/Assignment1Q1.java
+++ Armstrong/src/Assignment1Q1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class ArmstrongOrNot {
+class ArmstrongOrNot {
 	public boolean armstrongCheck(int num) {
         int t1, d = 0, l = 0, sum = 0;
 
@@ -19,20 +19,17 @@ public class ArmstrongOrNot {
         }
         return num == sum;
     }
-	public static class Assignment1Q1 {
+}
+	public class Assignment1Q1 {
 		public static void main(String[] args) {
 			int num;
-            Scanner sc = new Scanner(System.in);
+            Scanner kp = new Scanner(System.in);
             System.out.print("Enter the Number to Check: ");
-            num = sc.nextInt();
+            num = kp.nextInt();
             ArmstrongOrNot obj = new ArmstrongOrNot();
             if (obj.armstrongCheck(num))
                 System.out.print("Armstrong");
             else System.out.println("Not an Armstrong");
         }
 	}
-		
-
-	}
-
 
